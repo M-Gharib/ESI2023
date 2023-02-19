ef24983bacd1b002c5a3319ba854b99ac109d57d
mohamad <mohamad.gharib@ut.ee>
2023-02-19 16:44:33 +0200

    week2v2

--- src/main/java/com/esi/week2/Week2Application.java
+++ src/main/java/com/esi/week2/Week2Application.java
@@ -1,13 +1,13 @@
-package com.esi.week1;
+package com.esi.week2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Week1Application {
+public class Week2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Week1Application.class, args);
+		SpringApplication.run(Week2Application.class, args);
 	}
 
 }
---
+++ src/main/java/com/esi/week2/products/Product.java
@@ -0,0 +1,65 @@
+package com.esi.week2.products;
+
+import java.math.BigDecimal;
+
+
+
+public class Product {
+    private String id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+
+
+
+
+public Product() {
+    }
+
+public Product(String id) {
+        this.id = id;
+    }
+
+public Product(String id, String name, String description, BigDecimal price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+
+public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+
+
+}
