42bcb9bd35ce6aae3031c8392d73102758ec14f8
mohamad <mohamad.gharib@ut.ee>
2023-03-06 12:44:19 +0200

    week5

--- src/main/java/com/esi/week4refactored/products/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.esi.week4refactored.products.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.esi.week4refactored.products.model.Product;
-
-public interface ProductRepository extends CrudRepository<Product, String>{
-
-}
